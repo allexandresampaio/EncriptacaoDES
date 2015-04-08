6ecde41ba48250753bdef6d57d9b787bbeb8e069
allexandresampaio <allexandresss@gmail.com>
2015-04-07 23:42:13 -0300

    Método de redução pelas tabelas

--- Encriptacao DES/src/DES/Bloco.java
+++ Encriptacao DES/src/DES/Bloco.java
@@ -112,9 +112,15 @@ public class Bloco {
         }
     }
     
-    public void f_reducao(int[] x, int flag){//reduz o bloco de 6 para 4 bits, de acordo com as tabelas de S1 a S8
+    public String f_reducao(int[] x, int flag){//reduz o bloco de 6 para 4 bits, de acordo com as tabelas de S1 a S8
         int[] pontas = {x[0], x[5]};
         int[] meio = {x[1], x[2], x[3], x[4]};
+        int linha = Integer.parseInt(pontas[0]+""+pontas[1]);
+        int coluna = Integer.parseInt(meio[0]+""+meio[1]+meio[2]+meio[3]);
+        String l = Integer.toString(linha, 2);
+        String c = Integer.toString(coluna, 2);
+        int retorno=0;
+        
         switch (flag){
             case 1:
                 int[][] S1 = {
@@ -122,10 +128,9 @@ public class Bloco {
                     {0, 15, 7, 4, 14, 2, 13, 1, 10, 6, 12, 11, 9, 5, 3, 8},
                     {4, 1, 14, 8, 13, 6, 2, 11, 15, 12, 9, 7, 3, 10, 5, 0},
                     {15, 12, 8, 2, 4, 9, 1, 7, 5, 11, 3, 14, 10, 0, 6, 13}};
-                    
-                    int linha = Integer.parseInt(pontas[0]+""+pontas[1]);
-                    int coluna = Integer.parseInt(meio[0]+""+meio[1]+meio[2]+meio[3]);
-                    
+                
+                retorno = S1[Integer.parseInt(l)][Integer.parseInt(c)];
+                break;
                 
             case 2:
                 int[][] S2 = {
@@ -133,42 +138,71 @@ public class Bloco {
                     {3, 13, 4, 7, 15, 2, 8, 14, 12, 0, 1, 10, 6, 9, 11, 5},
                     {0, 14, 7, 11, 10, 4, 13, 1, 5, 8, 12, 6, 9, 3, 2, 15},
                     {13, 8, 10, 1, 3, 15, 4, 2, 11, 6, 7, 12, 0, 5, 14, 9}};
+                
+                retorno = S2[Integer.parseInt(l)][Integer.parseInt(c)];
+                break;
+                
             case 3:
                 int[][] S3 = {
                     {10, 0, 9, 14, 6, 3, 15, 5, 1, 13, 12, 7, 11, 4, 2, 8},
                     {13, 7, 0, 9, 3, 4, 6, 10, 2, 8, 5, 14, 12, 11, 15, 1},
                     {13, 6, 4, 9, 8, 15, 3, 0, 11, 1, 2, 12, 5, 10, 14, 7},
                     {1, 10, 13, 0, 6, 9, 8, 7, 4, 15, 14, 3, 11, 5, 2, 12}};
+                
+                retorno = S3[Integer.parseInt(l)][Integer.parseInt(c)];
+                break;
+                
             case 4:
                 int[][] S4 = {
                     {7, 13, 14, 3, 0, 6, 9, 10, 1, 2, 8, 5, 11, 12, 4, 15},
                     {13, 8, 11, 5, 6, 15, 0, 3, 4, 7, 2, 12, 1, 10, 14, 9},
                     {10, 6, 9, 0, 12, 11, 7, 13, 15, 1, 3, 14, 5, 2, 8, 4},
                     {3, 15, 0, 6, 10, 1, 13, 8, 9, 4, 5, 11, 12, 7, 2, 14}};
+                
+                retorno = S4[Integer.parseInt(l)][Integer.parseInt(c)];
+                break;
+                
             case 5:
                 int[][] S5 = {
                     {2, 12, 4, 1, 7, 10, 11, 6, 8, 5, 3, 15, 13, 0, 14, 9},
                     {14, 11, 2, 12, 4, 7, 13, 1, 5, 0, 15, 10, 3, 9, 8, 6},
                     {4, 2, 1, 11, 10, 13, 7, 8, 15, 9, 12, 5, 6, 3, 0, 14},
                     {11, 8, 12, 7, 1, 14, 2, 13, 6, 15, 0, 9, 10, 4, 5, 3}};
+                
+                retorno = S5[Integer.parseInt(l)][Integer.parseInt(c)];
+                break;
+                
             case 6:
                 int[][] S6 = {
                     {12, 1, 10, 15, 9, 2, 6, 8, 0, 13, 3, 4, 14, 7, 5, 11},
                     {10, 15, 4, 2, 7, 12, 9, 5, 6, 1, 13, 14, 0, 11, 3, 8},
                     {9, 14, 15, 5, 2, 8, 12, 3, 7, 0, 4, 10, 1, 13, 11, 6},
                     {4, 3, 2, 12, 9, 5, 15, 10, 11, 14, 1, 7, 6, 0, 8, 13}};
+                
+                retorno = S6[Integer.parseInt(l)][Integer.parseInt(c)];
+                break;
+                
             case 7:
                 int[][] S7 = {
                     {4, 11, 2, 14, 15, 0, 8, 13, 3, 12, 9, 7, 5, 10, 6, 1},
                     {13, 0, 11, 7, 4, 9, 1, 10, 14, 3, 5, 12, 2, 15, 8, 6},
                     {1, 4, 11, 13, 12, 3, 7, 14, 10, 15, 6, 8, 0, 5, 9, 2},
                     {6, 11, 13, 8, 1, 4, 10, 7, 9, 5, 0, 15, 14, 2, 3, 12}};
+                
+                retorno = S7[Integer.parseInt(l)][Integer.parseInt(c)];
+                break;
+                
             case 8:
                 int[][] S8 = {
                     {13, 2, 8, 4, 6, 15, 11, 1, 10, 9, 3, 14, 5, 0, 12, 7},
                     {1, 15, 13, 8, 10, 3, 7, 4, 12, 5, 6, 11, 0, 14, 9, 2},
                     {7, 11, 4, 1, 9, 12, 14, 2, 0, 6, 10, 13, 15, 3, 5, 8},
                     {2, 1, 14, 7, 4, 10, 8, 13, 15, 12, 9, 0, 3, 5, 6, 11}};
+                
+                retorno = S8[Integer.parseInt(l)][Integer.parseInt(c)];
+                break;
         }
+        
+        return Integer.toString(retorno, 2);
     } 
 }
