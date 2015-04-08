3f4683f2d94cf3233caccd89e1c1ce9dd7e73e46
allexandresampaio <allexandresss@gmail.com>
2015-04-08 18:25:43 -0300

    classe main finalizada

--- Encriptacao DES/src/DES/Main.java
+++ Encriptacao DES/src/DES/Main.java
@@ -13,11 +13,12 @@ import java.util.ArrayList;
  */
 public class Main {
     
-    public void mMain(String mensagem) {
+    public ArrayList<String> mMain(String mensagem, ArrayList<String> chaveRounds) {
         Mensagem principal = new Mensagem();//instancia um objeto mensagem p/ usar os métodos
         String mensagemBin = principal.converteAscBin(mensagem);//pega a mensagem recebida e coloca em binário
         ArrayList<String> blocos = new ArrayList<>();//cria um array para guardar os blocos
         blocos = principal.divide64(mensagemBin);//coloca os blocos no array
+        ArrayList<String> retorno = new ArrayList<>();
         
         for (int i=0; i<blocos.size(); i++){
             int[] itens = new int[8];
@@ -27,12 +28,33 @@ public class Main {
             Bloco bloco = new Bloco(itens);
             bloco = bloco.permuta_PI();
             bloco.divisao();
-            bloco.setD(bloco.f_expansao());
-            bloco = f_XOR(bloco.getD(), bloco.getE());
+            
+            for (int k=0; k<16; k++){
+                int[] chave = new int[48];
+                for (int l=0; l<48; l++){
+                    chave[i]=chaveRounds.get(k).charAt(l);//pega os itens de cada bloco e coloca no array de inteiros.
+                }
+                bloco.setD(bloco.f_expansao());
+                bloco = f_XOR(bloco.getD(), chave);
+                ArrayList<int[]> blocosDe6 = new ArrayList<>();
+                blocosDe6 = bloco.f_divisao();
+                int[] blocosDe6reag = new int[32];
+                for (int m=0; m<8; m++){
+                    bloco.f_reducao(blocosDe6.get(m), m+1);
+                }
+                blocosDe6reag = bloco.conversao_S();
+                blocosDe6reag = bloco.despermuta_PI(blocosDe6reag);
+                bloco.aux=blocosDe6reag;
+                bloco.setD(bloco.getE());
+                bloco.setE(bloco.aux);
+            }
+        
+            retorno.add(String.valueOf(bloco.getD()));
+            retorno.add(String.valueOf(bloco.getE()));
         }
-
+        return retorno;
     }
-    
+
     public Bloco f_XOR(int[] entrada, int[] chave){//faz o XOR entre o bloco de entrada e a chave
         Bloco retorno = new Bloco();
         int[] ret = new int[48];
